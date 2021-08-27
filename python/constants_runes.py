9cf4c0e3e456813cd38b544d5756bc65a307ed1c
monalivadje <81947846+monalivadje@users.noreply.github.com>
2021-08-27 13:37:43 +0530

    Create constants_runes.py

---
+++ python/constants_runes.py
@@ -0,0 +1,18 @@
+""" constants value used in implementation of runes driver """
+
+   grav_acc = 9.80665
+   r_gas_dry = 287.026
+   cp_air_dry = 1.005e+03
+   pi = 3.14159265358979323846
+   seconds_per_day = 8.6400e+04
+   co2_mix_ratio = 6.0e-4
+   ch4_mix_ratio = 9.637e-7
+   n2o_mix_ratio = 4.7255e-7
+   o2_mix_ratio = 0.23139
+   t_ground(n_profile) = 294.0
+   solar_irrad(n_profile) = 1361.0
+   grey_albedo_sw = 0.06
+   grey_albedo_lw = 0.0
+   cos_zenith_angle(n_profile) = cos(45.0*3.14159/180.0)
+   n_profile=1
+   n_layer=32
