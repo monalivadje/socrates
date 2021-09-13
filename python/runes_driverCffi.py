8e0ef24dce23d43b946ad5b2900bd6b4c084978b
monalivadje <81947846+monalivadje@users.noreply.github.com>
2021-09-13 18:25:16 +0530

    Update runes_driverCffi.py

--- python/runes_driverCffi.py
+++ python/runes_driverCffi.py
@@ -20,7 +20,7 @@ def numpy_pointer(numpy_array,ffi):
 def main():
 
  ffi = cffi.FFI()
- ffibuilder = FFI()
+ ffibuilder = cffi.FFI()
  ffibuilder.cdef("void runes_wrapper( int n_profile , int n_layer , char *spectrum_name , double i_source , float *p_layer, float *t_layer , float *mass , float *density , float *layer_heat_capacity , float *h2o , float *o3 , float co2_mix_ratio , float n2o_mix_ratio , float ch4_mix_ratio , float o2_mix_ratio , float cos_zenith_angle , float solar_irrad , bool l_grey_albedo, float grey_albedo , bool l_rayleigh , bool l_invert ,float *heating_rate, float *flux_up, float *flux_down);") 
   # ffibuilder.cdef("void set_spectrum( char *spectrum_name , char *spectral_file , bool l_all_gases);")
 
