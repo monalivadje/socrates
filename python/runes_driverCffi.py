d9d271b78573ae8da94c822c60ed94a3f87e6c73
monalivadje <81947846+monalivadje@users.noreply.github.com>
2021-09-14 18:26:49 +0530

    Update runes_driverCffi.py

--- python/runes_driverCffi.py
+++ python/runes_driverCffi.py
@@ -1,16 +1,12 @@
 
 # python interface to fortran module runes_driver using cffi
 
-import os
 import numpy as np
 from constants_runes import grav_acc,r_gas_dry,cp_air_dry,pi,seconds_per_day , co2_mix_ratio , ch4_mix_ratio ,n2o_mix_ratio, o2_mix_ratio, n_profile, n_layer, t_ground,solar_irrad,grey_albedo_sw ,grey_albedo_lw 
 import cffi
 from cffi import FFI
-from os.path import abspath
+import runesdri
 
-runes_library_path = abspath('./libicore.a')
-runeslib = os.path.abspath(runes_library_path)
-desired_compiler = 'gcc'
 
 def numpy_pointer(numpy_array,ffi):
     assert numpy_array.flags['F_CONTIGUOUS'], \
@@ -21,16 +17,7 @@ def main():
 
  ffi = cffi.FFI()
  ffibuilder = cffi.FFI()
- ffibuilder.cdef("void runes_wrapper( int n_profile , int n_layer , char *spectrum_name , double i_source , float *p_layer, float *t_layer , float *mass , float *density , float *layer_heat_capacity , float *h2o , float *o3 , float co2_mix_ratio , float n2o_mix_ratio , float ch4_mix_ratio , float o2_mix_ratio , float cos_zenith_angle , float solar_irrad , bool l_grey_albedo, float grey_albedo , bool l_rayleigh , bool l_invert ,float *heating_rate, float *flux_up, float *flux_down);") 
-  # ffibuilder.cdef("void set_spectrum( char *spectrum_name , char *spectral_file , bool l_all_gases);")
-
- #if not os.environ.get('CC'):
- #   os.environ['CC'] = desired_compiler
-
- ffibuilder.set_source("runesdri", r''' #include "runes.h" ''', library_dirs=[os.getcwd()], include_dirs=[os.getcwd()],extra_link_args=[os.path.abspath(runes_library_path), '-lgfortran'])
- #lib_runes = ffi.dlopen(runeslib)
-
-
+ 
   p_layer = np.asfortranarray([[0.337000E+01, 0.509050E+02, 0.135550E+03, 0.254500E+03],[0.492500E+03,0.986000E+03, 0.204500E+04, 0.299500E+04],[0.349000E+04, 0.406500E+04, 0.473500E+04, 0.552500E+04],[0.645000E+04, 0.753500E+04, 0.881000E+04, 0.103000E+05],[0.120500E+05, 0.141500E+05, 0.166000E+05, 0.194000E+05],[0.226000E+05, 0.262000E+05, 0.302500E+05,0.348000E+05],[0.399000E+05, 0.456500E+05, 0.520500E+05, 0.591000E+05],[0.669000E+05, 0.756000E+05, 0.852000E+05, 0.957500+05]])
      
 
@@ -66,7 +53,6 @@ def main():
    print(sw_heating_rate) 
    print(sw_flux_up) 
    print(sw_flux_down)
-
 
   runesdri.lib.runes_wrapper(n_profile ,n_layer , spectrum_name , i_source , numpy_pointer(p_layer, ffi), numpy_pointer(t_layer,ffi) , numpy_pointer(mass,ffi) , numpy_pointer(density,ffi) ,numpy_pointer(layer_heat_capacity,ffi) , numpy_pointer(h2o,ffi), numpy_pointer(o3,ffi), co2_mix_ratio , n2o_mix_ratio , ch4_mix_ratio ,o2_mix_ratio , cos_zenith_angle , solar_irrad , l_grey_albedo, grey_albedo, l_rayleigh , l_invert, numpy_pointer(lw_heating_rate,ffi), numpy_pointer(lw_flux_up,ffi),numpy_pointer(lw_flux_down,ffi))
 
